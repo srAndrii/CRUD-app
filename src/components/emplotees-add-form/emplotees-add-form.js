f3cf42e13ad668e09d67e8cc1b79ecdc6f6d6c7c
Andrii <55akif5548@gmail.com>
2022-03-17 14:34:37 +0200

    Работа с формами, добавлены  управляемые компоненты

--- src/components/emplotees-add-form/emplotees-add-form.js
+++ src/components/emplotees-add-form/emplotees-add-form.js
@@ -1,23 +1,44 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
+class EmployeesAddForm extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            salary:0
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render() {
+        return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?" 
+                    name="name"
+                    onChange={this.onValueChange}/>
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+                    placeholder="З/П в $?" 
+                    name="salary"
+                    onChange={this.onValueChange}/>
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
     )
+    }
 }
 
 export default EmployeesAddForm;
