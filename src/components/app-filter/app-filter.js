8eada336f63b32dcd41acbe41ea814fd4214838a
Andrii <55akif5548@gmail.com>
2022-03-18 11:37:47 +0200

    Реализован поиск и фильтры

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,29 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        { name: 'all', lable: 'Все сотрудники' },
+        { name: 'rise', lable: 'На повышение' },
+        { name: 'moreThen1000', lable: 'ЗП больше 100$' }
+    ];
+
+    const buttons = buttonsData.map(({ name, lable }) => {
+        const active = props.filter === name;
+        const clazz= active ? 'btn-light' : 'btn-outline-light'
+        return (
+            <button
+                className={`btn ${clazz}`}
+                type="button"
+                key={name}
+                onClick={()=> props.onFilterSelect(name)} >
+                    {lable}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button
-                className="btn btn-light"
-                type="button">
-                    Все сотрудники
-            </button>
-            <button
-                className="btn btn-outline-light"
-                type="button">
-                    На повышение
-            </button>
-            <button
-                className="btn btn-outline-light"
-                type="button">
-                    ЗП больше 100$
-            </button>
+            {buttons} 
         </div>
     )
 }
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,49 +1,29 @@
 
-import { Component } from 'react';
+
 import './employees-list-item.css';
 
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            rise:false
-        }
+const EmployeesListItem=(props)=>  {
+    const { name, salary, onDelete, onToggleProp, increase, rise } = props;
+
         
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+    classNames += ' increase';
     }
-    onIncrease = () => {
-        this.setState(({ increase }) => ({
-            increase: !increase
-        }))
-    }
-    onRise = () => {
-        this.setState(({ rise }) => ({
-            rise: !rise
-        }))
+    if (rise) {
+        classNames += ' like'
     }
 
-    render() {
-        const { name, salary, onDelete} = this.props;
-        const { increase } = this.state;
-        const { rise } = this.state;
-        
-        let classNames = "list-group-item d-flex justify-content-between";
-        if (increase) {
-        classNames += ' increase';
-        }
-        if (rise) {
-            classNames += ' like'
-        }
- 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise" >{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary+ '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
-                    onClick={this.onIncrease}>
+                    onClick={onToggleProp}
+                    data-toggle="increase">
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -56,6 +36,6 @@ class EmployeesListItem extends Component {
             </div>
         </li>
     )
-    }
+    
 }
 export default EmployeesListItem;
